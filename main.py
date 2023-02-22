e5723bbec27348a685f5f121e9bdb922a5b70b57
ka1serrr <86408343+ka1serrr@users.noreply.github.com>
2023-02-22 18:46:04 +0300

    project is done

--- main.py
+++ main.py
@@ -1,38 +1,94 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 
-# file = 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-07/coffee_ratings.csv'
 #TODO : сравнить средние баллы двух (мб и больше) видов кофе
 file = 'coffee_ratings.csv'
 coffee = pd.read_csv(file, delimiter=',')
 new_coffee_list = coffee.rename(columns={'species': 'Виды_кофе','total_cup_points': 'Количество_очков' })
-# print(coffee[['total_cup_points', 'species']][:3])
-# print(len(coffee))
+
+def average_points(length: int, sum_of_points: int):
+    return sum_of_points // length
 
 
-def average_points(length: int, sum_of_points: int, type_of_cofffee: str):
-    print(f'Средний бал кофе {type_of_cofffee} -', sum_of_points // length)
-    return (sum_of_points // length)
+def min_points(lit_of_coffee):
+    return lit_of_coffee.min()
+
+
+def max_points(list_of_coffee):
+    return list_of_coffee.max()
 
 
 def statistics(length: int, type_of_coffee: str, list_of_coffee):
-    average_points(length, list_of_coffee['Количество_очков'].sum(), type_of_coffee)
-    print(f'Минимальное значение {list_of_coffee["Количество_очков"].min()}')
+    print(f'Максимальное значение {type_of_coffee} {max_points(list_of_coffee["Количество_очков"])}')
+    print(f'Минимальное значение {type_of_coffee} {min_points(list_of_coffee["Количество_очков"])}')
+    print(f'Среднее значение {type_of_coffee} {average_points(length, list_of_coffee["Количество_очков"].sum())}')
 
+
+def show_max_graph(max_robusta, max_arabica):
+    plt.title('Максимальное')
+    plt.xlabel('Виды кофе')
+    plt.ylabel('Кол-во балов')
+
+    robusta_list = ['Robusta']
+    arabica_list = ['Arabica']
+    plt.bar(robusta_list, np.float_(max_robusta), label='Robusta')
+    plt.bar(arabica_list, np.float_(max_arabica), label='Arabica')
+
+
+def show_min_graph(min_robusta, min_arabica):
+    plt.title('Минимальное')
+    plt.xlabel('Виды кофе')
+    plt.ylabel('Кол-во балов')
+
+    robusta_list = ['Robusta']
+    arabica_list = ['Arabica']
+    plt.bar(robusta_list, np.float_(min_robusta), label='Robusta')
+    plt.bar(arabica_list, np.float_(min_arabica), label='Arabica')
+
+
+def show_avg_graph(avg_robusta, avg_arabica):
+    plt.title('Среднее')
+    plt.xlabel('Виды кофе')
+    plt.ylabel('Кол-во балов')
+
+    robusta_list = ['Robusta']
+    arabica_list = ['Arabica']
+    plt.bar(robusta_list, np.float_(avg_robusta), label='Robusta')
+    plt.bar(arabica_list, np.float_(avg_arabica), label='Arabica')
+
+
+def show_graphs(max_robusta, max_arabica, min_robusta, min_arabica, avg_robusta, avg_arabica):
+    plt.figure()
+
+    plt.subplot(1, 3, 1)
+    show_max_graph(max_robusta, max_arabica)
+    plt.legend()
+
+    plt.subplot(1, 3, 2)
+    show_min_graph(min_robusta, min_arabica)
+    plt.legend
+
+    plt.subplot(1, 3, 3)
+    show_avg_graph(avg_robusta, avg_arabica)
+    plt.legend()
+
+    plt.show()
 
 def read_coffee_file(csv: str):
     print(f'Всего кофе {len(coffee)}')
     filterd_file = csv[['Количество_очков', 'Виды_кофе']]
     # Решено взять только 29, так как количество Робусты сильно меньше, чем Арабики.
+
     Arabica = filterd_file[filterd_file['Виды_кофе'] == 'Arabica'][:28]
     Robusta = filterd_file[filterd_file['Виды_кофе'] == 'Robusta']
     len_Arabica = len(Arabica)
     len_Rosuta = len(Robusta)
+
     statistics(len_Arabica, 'Arabica', Arabica)
-    # average_points(len_Arabica, Arabica['Количество_очков'].sum(), 'Arabica')
-    # average_points(len_Rosuta, Robusta['Количество_очков'].sum(), 'Robusta')
+    statistics(len_Rosuta, 'Robusta', Robusta)
+    # show_max_graph(max_points(Robusta['Количество_очков']), max_points(Arabica['Количество_очков']))
 
-    print(Arabica)
-
+    show_graphs(max_points(Robusta['Количество_очков']), max_points(Arabica['Количество_очков']), min_points(Robusta['Количество_очков']), min_points(Arabica['Количество_очков']), average_points(len_Rosuta, Robusta['Количество_очков'].sum()), average_points(len_Arabica, Arabica['Количество_очков'].sum()))
 
 read_coffee_file(new_coffee_list)
