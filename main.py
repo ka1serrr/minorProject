4c7f4a0324f52d25c4720036ffbc626974a373c2
ka1serrr <86408343+ka1serrr@users.noreply.github.com>
2023-03-19 19:37:40 +0300

    finish

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-#TODO : сравнить средние баллы двух (мб и больше) видов кофе
+#TODO: сравнить средние баллы двух видов кофе (Арабика и Робуста)
+#ГИПОТЕЗА:  Арабика, как самый популярный вид кофе будет более вкусным (т. е. в условиях этого небольшого исследования получит больший бал, чем Робуста)
+#Из дальнейшего исследования можно будет заметить, что в csv файле 1339 строк, только 29 из которых относятся к Робусте.
+
+
+
 file = 'coffee_ratings.csv'
 coffee = pd.read_csv(file, delimiter=',')
 new_coffee_list = coffee.rename(columns={'species': 'Виды_кофе','total_cup_points': 'Количество_очков' })
@@ -75,20 +80,30 @@ def show_graphs(max_robusta, max_arabica, min_robusta, min_arabica, avg_robusta,
 
     plt.show()
 
+
+
 def read_coffee_file(csv: str):
     print(f'Всего кофе {len(coffee)}')
     filterd_file = csv[['Количество_очков', 'Виды_кофе']]
-    # Решено взять только 29, так как количество Робусты сильно меньше, чем Арабики.
+    # Для более объективной оценки, решено взять только первые 29, так как количество Робусты сильно меньше, чем Арабики.
 
+    # Получаем срез Арабики
     Arabica = filterd_file[filterd_file['Виды_кофе'] == 'Arabica'][:28]
+
+    # Получаем срез Робусты
     Robusta = filterd_file[filterd_file['Виды_кофе'] == 'Robusta']
+
+    # Получение длину срезов.
     len_Arabica = len(Arabica)
     len_Rosuta = len(Robusta)
 
+    # Получаем статистику по двум видам кофе (Минимальное, максимальное, среднее кол-во балов)
     statistics(len_Arabica, 'Arabica', Arabica)
     statistics(len_Rosuta, 'Robusta', Robusta)
-    # show_max_graph(max_points(Robusta['Количество_очков']), max_points(Arabica['Количество_очков']))
+
 
     show_graphs(max_points(Robusta['Количество_очков']), max_points(Arabica['Количество_очков']), min_points(Robusta['Количество_очков']), min_points(Arabica['Количество_очков']), average_points(len_Rosuta, Robusta['Количество_очков'].sum()), average_points(len_Arabica, Arabica['Количество_очков'].sum()))
 
 read_coffee_file(new_coffee_list)
+
+#Исходя из всего вышеуказанноо, можно сделать вывод, что Арабика, как и предполагалось, получила больше баллов (и максимаьных, и минимальных, и средних; 90 : 83, 86: 73, 87 : 80), чем Робуста
